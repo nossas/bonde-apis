30c822adc6fa2d6cd4383f6416160ebd031c0aed
Igor Santos <igr.exe@gmail.com>
2020-08-03 17:17:47 -0300

    chore: add example api graphql

---
+++ examples/package-api/src/schema/index.ts
@@ -0,0 +1,9 @@
+import 'graphql-import-node';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { GraphQLSchema } from 'graphql';
+import * as typeDefs from './schema.graphql';
+import resolvers from '../resolvers';
+
+const schema: GraphQLSchema = makeExecutableSchema({ typeDefs, resolvers });
+
+export default schema;
