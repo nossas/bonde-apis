b599d1cd7c8f23e821bbf1dcbc496b9e93c3382b
Viviane Dias <vivianedias@protonmail.com>
2020-11-18 17:41:20 -0300

    chore: adds more envs in configs

--- packages/redes-api/src/config.ts
+++ packages/redes-api/src/config.ts
@@ -9,6 +9,11 @@ type Config = {
   hasuraSecret?: string
   graphqlHttpUrl?: string
   jwtToken?: string
+  jwtSecret?: string
+  zendeskApiToken?: string
+  zendeskApiUrl?: string
+  zendeskApiUser?: string
+  zendeskOrganizations?: string
 }
 
 const config: Config = {
@@ -17,7 +22,12 @@ const config: Config = {
   logLevel: process.env.LOG_LEVEL || 'info',
   hasuraSecret: process.env.HASURA_SECRET,
   graphqlHttpUrl: process.env.GRAPHQL_HTTP_URL,
-  jwtToken: process.env.JWT_TOKEN
+  jwtToken: process.env.JWT_TOKEN,
+  jwtSecret: process.env.JWT_SECRET,
+  zendeskApiToken: process.env.ZENDESK_API_TOKEN,
+  zendeskApiUrl: process.env.ZENDESK_API_URL,
+  zendeskApiUser: process.env.ZENDESK_API_USER,
+  zendeskOrganizations: process.env.ZENDESK_ORGANIZATIONS
 };
 
 export default config;
