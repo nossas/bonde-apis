2eebeb99c587b767889c5efeadf38be8f1fe7880
Viviane Dias <vivianedias@protonmail.com>
2020-11-18 13:04:37 -0300

    feat(redes): match query

---
+++ packages/redes-api/src/types/match.ts
@@ -0,0 +1,8 @@
+export type MatchTicket = {
+  individuals_ticket_id: number
+  volunteers_ticket_id: number
+  individuals_user_id: number
+  volunteers_user_id: number
+  community_id: number
+  status: string
+}
