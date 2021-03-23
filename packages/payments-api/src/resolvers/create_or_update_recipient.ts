f61d1f7d811cbe04204c559f3e415693e71f36de
Igor Santos <igr.exe@gmail.com>
2021-03-23 20:33:24 -0300

    fix(payments): Limit to 30 chars recipient bank legal name sent to pagarme

--- packages/payments-api/src/resolvers/create_or_update_recipient.ts
+++ packages/payments-api/src/resolvers/create_or_update_recipient.ts
@@ -40,38 +40,44 @@ type Args = {
 }
 
 const create_or_update = async (_: void, args: Args): Promise<RecipientEntity | undefined> => {
-  const { input: { id, recipient, community_id } } = args;
-  const client: any = await pagarme.client.connect({ api_key: config.pagarmeApiKey });
+  try {
+    const { input: { id, recipient, community_id } } = args;
+    const client: any = await pagarme.client.connect({ api_key: config.pagarmeApiKey });
 
-  
-  if (!!id) {
-    // The next line ensures only 1 recipient by Community on Pagarme
-    if (!recipient.id) throw new Error('recipient.id is required');
+    recipient.bank_account.legal_name = recipient.bank_account.legal_name.substring(0, 29);
+    
+    if (!!id) {
+      // The next line ensures only 1 recipient by Community on Pagarme
+      if (!recipient.id) throw new Error('recipient.id is required');
 
-    const pagarmeUpdated: Recipient = await client.recipients.update(recipient);
+      const pagarmeUpdated: Recipient = await client.recipients.update(recipient);
 
-    await recipients.update({
-      recipient: pagarmeUpdated,
-      pagarme_recipient_id: pagarmeUpdated.id,
-      transfer_day: pagarmeUpdated.transfer_day,
-      transfer_enabled: pagarmeUpdated.transfer_enabled
-    }, id);
+      await recipients.update({
+        recipient: pagarmeUpdated,
+        pagarme_recipient_id: pagarmeUpdated.id,
+        transfer_day: pagarmeUpdated.transfer_day,
+        transfer_enabled: pagarmeUpdated.transfer_enabled
+      }, id);
 
-    return { id, recipient: pagarmeUpdated, community_id };
+      return { id, recipient: pagarmeUpdated, community_id };
+    }
+
+    // Insert Recipient on Pagarme
+    const pagarmeCreated: Recipient = await client.recipients.create(recipient);
+    // Insert Recipient on Bonde database (API GraphQL)
+    const bondeCreated = await recipients.insert({
+      recipient: pagarmeCreated,
+      pagarme_recipient_id: pagarmeCreated.id,
+      transfer_day: pagarmeCreated.transfer_day,
+      transfer_enabled: pagarmeCreated.transfer_enabled,
+      community_id
+    });
+    
+    return { id: bondeCreated.id, recipient: pagarmeCreated, community_id };
+  } catch (err) {
+    if (err.response) throw new Error(err.response);
+    else throw new Error(err);    
   }
-
-  // Insert Recipient on Pagarme
-  const pagarmeCreated: Recipient = await client.recipients.create(recipient);
-  // Insert Recipient on Bonde database (API GraphQL)
-  const bondeCreated = await recipients.insert({
-    recipient: pagarmeCreated,
-    pagarme_recipient_id: pagarmeCreated.id,
-    transfer_day: pagarmeCreated.transfer_day,
-    transfer_enabled: pagarmeCreated.transfer_enabled,
-    community_id
-  });
-  
-  return { id: bondeCreated.id, recipient: pagarmeCreated, community_id };
 }
 
 export default check_user(create_or_update, Roles.ADMIN);
