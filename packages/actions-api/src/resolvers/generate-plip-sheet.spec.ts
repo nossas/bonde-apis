56b81ab4c85987be6dbab4e6f9a2cd9a10de4ce7
Adriele A Secundino <dri.leela@gmail.com>
2021-11-29 15:13:48 -0300

    chore: update test

--- packages/actions-api/src/resolvers/generate-plip-sheet.spec.ts
+++ packages/actions-api/src/resolvers/generate-plip-sheet.spec.ts
@@ -3,9 +3,11 @@ import generatePlipSheet from "./generate_plip_sheet"
 describe("generate data file with unique_identifier", () => {
 
   const body = {
-    input: {
+      email: "teste@email.com",
+      state: "",
+      form_data: "",
       unique_identifier: '0b30ce76-f321-4c71-9168-b6bc98d843ee'
-    }
+    
   }
   it("should return file data", async () => {
     const data = await generatePlipSheet(body);
