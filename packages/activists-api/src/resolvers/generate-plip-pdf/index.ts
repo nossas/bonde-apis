fc0d50addb47fa1938d976c396f4e913c5c7c2ad
Adriele A Secundino <dri.leela@gmail.com>
2021-12-03 17:28:14 -0300

    chore(activists-api): add form to pdf plip

--- packages/activists-api/src/resolvers/generate-plip-pdf/index.ts
+++ packages/activists-api/src/resolvers/generate-plip-pdf/index.ts
@@ -29,19 +29,27 @@ const generatePlipPdf = async (unique_identifier: string, state : string): Promi
   doc.setFont( "helvetica" ,"bold");
   doc.text( `Lista de Apoio ao Projeto de Lei de Iniciativa Popular nº13.567`,256,15, { align:'center' });
   doc.setFont( "helvetica", 'normal');
+  doc.setFontSize(9.5);
   doc.text( `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet
     dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
     lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit.\nSaiba mais em www.sitedaplip.org`,256,25, { align:'center' });
   //doc.setFont( "helvetica" ,"bold");
   //doc.text( `www.sitedaplip.org`,45,51, { align:'center' });
-  doc.setFontSize(7.5);
+  doc.setFontSize(6);
   doc.setFont( "helvetica", 'bold');
   doc.cell(10,60,(docWidth-20)/2,12,`ESTADO: ${state}`,0,'left');
   doc.cell((docWidth-20),60,(docWidth-20)/2,12,`MUNICIPIO:`,0,'left');
-  doc.cell(10,100,90,10,``,1,'left');
-    
-  const formData =  imageDataUri('./plip-form.png');
-  doc.addImage(formData, 'PNG', 8, 80,432,540);
+  doc.cell(10,100,90,5,``,1,'left');
+  for (var i = 0; i < 10; i++) {
+  doc.cell(10,110,(docWidth-120),18,`NOME:`,2,'left');
+  doc.cell((docWidth-120),110,100,54,`ASSINATURA DIGITAL`,2,'right');
+  doc.cell(10,134,(docWidth-120),18,``,2,'left');  
+  doc.cell(10,134,(docWidth-120),18,`ENDEREÇO:`,3,'left');
+  doc.cell(10,158,(docWidth-120)/4,18,`DATA DE NASCIMENTO: `,4,'left');
+  doc.cell((docWidth-120)/4,158,(docWidth-120)-((docWidth-120)/4),18,`TITULO DE ELEITOR:`,4,'left');
+  }
+  //const formData =  imageDataUri('./plip-form.png');
+ // doc.addImage(formData, 'PNG', 8, 80,432,540);
     
   /*for (var i = 3; i <= 10; i++) {
       doc.setFontSize(6);
@@ -52,7 +60,7 @@ const generatePlipPdf = async (unique_identifier: string, state : string): Promi
   doc.setFont( "helvetica" ,"normal");
   doc.text( `${unique_identifier}`,10 , docHeight - 5);
   doc.text( `Enviar para: Nome do Destinatário, Av. N2 - Bloco 16, CEP 70165-900, DF`,docWidth -183 , docHeight - 5);
-  //doc.save('./output-2.pdf')
+ 
   return await deferred.resolve(doc.output('datauristring'));
 }
 export default generatePlipPdf;
