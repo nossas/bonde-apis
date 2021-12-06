015e3c7eba8434117b65197c23ee2ddf5a47bd9c
Adriele A Secundino <dri.leela@gmail.com>
2021-12-06 11:48:18 -0300

    chore(activists): organize the code and review the fields texts

--- packages/activists-api/src/resolvers/generate-plip-pdf/index.ts
+++ packages/activists-api/src/resolvers/generate-plip-pdf/index.ts
@@ -22,9 +22,15 @@ const generatePlipPdf = async (unique_identifier: string, state : string): Promi
   const doc = new jsPDF('p', 'px', 'a4');
   const docWidth = doc.internal.pageSize.width;
   const docHeight = doc.internal.pageSize.height;
-  const logoData = imageDataUri('./logo.jpg');
-  doc.addImage(logoData, 'JPEG', 10, 5,56,48);
+  const margin = 10; 
+  const cellHeight = 18;
+  const formWidth = docWidth-(2*margin);
+  const cellSignatureHeight = 100;
 
+  const logoData = imageDataUri('./logo.jpg');
+  
+  //header
+  doc.addImage(logoData, 'JPEG', 10, 5,56,48);
   doc.setFontSize(10);
   doc.setFont( "helvetica" ,"bold");
   doc.text( `Lista de Apoio ao Projeto de Lei de Iniciativa Popular nº13.567`,256,15, { align:'center' });
@@ -33,29 +39,36 @@ const generatePlipPdf = async (unique_identifier: string, state : string): Promi
   doc.text( `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet
     dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
     lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit.\nSaiba mais em www.sitedaplip.org`,256,25, { align:'center' });
-  //doc.setFont( "helvetica" ,"bold");
-  //doc.text( `www.sitedaplip.org`,45,51, { align:'center' });
+  
+  //form
   doc.setFontSize(6);
   doc.setFont( "helvetica", 'bold');
-  doc.cell(10,60,(docWidth-20)/2,12,`ESTADO: ${state}`,0,'left');
-  doc.cell((docWidth-20),60,(docWidth-20)/2,12,`MUNICIPIO:`,0,'left');
-  doc.cell(10,100,90,5,``,1,'left');
-  for (var i = 0; i < 10; i++) {
-  doc.cell(10,110,(docWidth-120),18,`NOME:`,2,'left');
-  doc.cell((docWidth-120),110,100,54,`ASSINATURA DIGITAL`,2,'right');
-  doc.cell(10,134,(docWidth-120),18,``,2,'left');  
-  doc.cell(10,134,(docWidth-120),18,`ENDEREÇO:`,3,'left');
-  doc.cell(10,158,(docWidth-120)/4,18,`DATA DE NASCIMENTO: `,4,'left');
-  doc.cell((docWidth-120)/4,158,(docWidth-120)-((docWidth-120)/4),18,`TITULO DE ELEITOR:`,4,'left');
-  }
-  //const formData =  imageDataUri('./plip-form.png');
- // doc.addImage(formData, 'PNG', 8, 80,432,540);
+  doc.cell(margin,60,(formWidth/2),12,`ESTADO: ${state}`,0,'left');
+  doc.cell(formWidth,60,formWidth/2,12,`MUNICIPIO:`,0,'left');
+  doc.cell(margin,100,90,5,``,1,'left');
+  
+  for (let i = 0; i < 10; i++) {
+    doc.cell(margin,110,(formWidth-cellSignatureHeight),
+      cellHeight,`NOME COMPLETO: (Por extenso e legível, sem abreviar)`,2,'left');
     
-  /*for (var i = 3; i <= 10; i++) {
-      doc.setFontSize(6);
-      doc.cell(docWidth -100,100,90,50,`ASSINATURA OU IMPRESSÃO DIGITAL`,i,'left');
-    }*/
+    doc.cell((formWidth-cellSignatureHeight ),110,100,54,
+      `ASSINATURA OU IMPRESSÃO DIGITAL`,2,'right');
+    doc.cell(margin,134,(formWidth-cellSignatureHeight),cellHeight,``,2,'left'); 
+
+    doc.cell(margin,134,(formWidth-cellSignatureHeight),
+      cellHeight,`ENDEREÇO: (Completo, legível, sem abreviar, com CEP)`,3,'left');
+
+    doc.cell(margin,158,(formWidth-cellSignatureHeight)/4,
+      cellHeight,`DATA DE NASCIMENTO: `,4,'left');
+
+    doc.cell((formWidth-cellSignatureHeight)/4,158,
+      (formWidth-cellSignatureHeight)-((formWidth-cellSignatureHeight)/4),
+      cellHeight,`NÚMERO DO TÍTULO DE ELEITOR: (Ou nome completo da mãe)`,4,'left');
+  }
+  
   const deferred = q;
+  
+  //footer
   doc.setFontSize(7);
   doc.setFont( "helvetica" ,"normal");
   doc.text( `${unique_identifier}`,10 , docHeight - 5);
