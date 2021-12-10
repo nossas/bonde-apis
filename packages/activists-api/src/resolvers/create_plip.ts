239b0db945019f64e52a80331a8f6c94914ab48e
Gloria Teodoro <gloriateodoro1@gmail.com>
2021-12-10 09:12:39 -0300

    fix: change pdf name

--- packages/activists-api/src/resolvers/create_plip.ts
+++ packages/activists-api/src/resolvers/create_plip.ts
@@ -3,16 +3,16 @@ import { PlipInput, IActionData, IBaseAction } from '../types';
 import * as ActionsAPI from '../graphql-api/actions';
 import makeActionResolver from './action';
 import { v4 as uuidv4 } from 'uuid';
-import  generatePlipPdf  from './generate-plip-pdf';
+import generatePlipPdf from './generate-plip-pdf';
 
 export const create_plip = async ({ action, widget }: IBaseAction<PlipInput>): Promise<IActionData> => {
-  
+
   const unique_identifier = uuidv4();
-  const state =  action?.state || '';
+  const state = action?.state || '';
   const expected_signatures = action?.expected_signatures || 10;
   const pdf_datauristring = await generatePlipPdf(unique_identifier, state, expected_signatures);
 
-  const { id , errors } = await ActionsAPI.plip({
+  const { id, errors } = await ActionsAPI.plip({
     widget_id: widget.id,
     //activist_id: activist.id,
     //community_id: widget.block.mobilization.community.id,
@@ -20,18 +20,18 @@ export const create_plip = async ({ action, widget }: IBaseAction<PlipInput>): P
     unique_identifier: unique_identifier,
     pdf_data: pdf_datauristring,
     expected_signatures: expected_signatures,
-    state: state ,
+    state: state,
     form_data: action || {}
   });
 
-  logger.child({ id, unique_identifier, errors } ).info('plip');
+  logger.child({ id, unique_identifier, errors }).info('plip');
 
   return {
-    data: { 
-      plip_id: id, 
+    data: {
+      plip_id: id,
       //transforma pdf data para base64 para anexar ao email
-      pdf_data: pdf_datauristring.replace("data:application/pdf;filename=generated.pdf;base64,", ""), 
-      filename: `formulario_plip_${action?.name}.pdf` 
+      pdf_data: pdf_datauristring.replace("data:application/pdf;filename=generated.pdf;base64,", ""),
+      filename: `${action?.name}.pdf`
     },
   };
 };
