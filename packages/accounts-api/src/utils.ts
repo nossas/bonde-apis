9ebb0aa32dbe08a72c23507e2d7c2116a53fcca7
Igor Santos <igr.exe@gmail.com>
2022-01-12 16:15:12 -0300

    feat(accounts): add hasura claims to generate jwt

--- packages/accounts-api/src/utils.ts
+++ packages/accounts-api/src/utils.ts
@@ -8,13 +8,38 @@ type User = {
 }
 
 export const generateJWT = (user: User): string => {
-  const payload = {
+  const payload: JWT = {
     sub: 'postgraphql',
     role: user.admin ? 'admin' : 'common_user',
     user_id: user.id,
-    is_admin: user.is_admin ? 1 : 0
+    is_admin: user.is_admin ? 1 : 0,
+    "https://hasura.io/jwt/claims": {
+      "x-hasura-allowed-roles": user.admin ? ['admin', 'common_user'] : ['common_user'],
+      "x-hasura-default-role": user.admin ? 'admin' : 'common_user',
+      "x-hasura-user-id": String(user.id)
+    }
   };
   const options = { audience: 'postgraphile' };
 
   return jwt.sign(payload, config.jwtSecret, options);
 };
+
+interface HasuraJWT {
+  "x-hasura-allowed-roles": string[]
+  "x-hasura-default-role": string
+  "x-hasura-user-id": string
+  // "x-hasura-org-id": "123",
+  // "x-hasura-custom": "custom-value"
+}
+
+
+interface JWT {
+  // Postgraphile Engine
+  sub: 'postgraphql'
+  role: 'admin' | 'common_user'
+  user_id: number
+  is_admin: 1 | 0 | boolean
+  // audience: 'postgraphile'
+  // Hasura Engine
+  "https://hasura.io/jwt/claims": HasuraJWT
+}
