4b4ec92d7abdeb2d164888821854a2eb5cc43f7d
Igor Santos <igr.exe@gmail.com>
2022-01-14 10:27:37 -0300

    feat(accounts): add cookie session to authenticate mutation

--- packages/accounts-api/src/resolvers/authenticate.ts
+++ packages/accounts-api/src/resolvers/authenticate.ts
@@ -8,16 +8,35 @@ type AuthenticateArgs = {
   password: string
 }
 
-export default async (_: void, args: AuthenticateArgs): Promise<JWT> => {
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export default async (_: void, args: AuthenticateArgs, { res }: any): Promise<JWT> => {
+  console.log("authenticate", { args, res });
   const { email, password } = args;
   const errorCode = 'email_password_dismatch';
+  let user;
 
-  const user = (await UsersAPI.find({ email }))[0];
+  try {
+    user = (await UsersAPI.find({ email }))[0];
+  } catch (err) {
+    console.log('authenticate error', err);
+  }
 
   if (!user) throw new Error(errorCode);
 
   if (await bcrypt.compare(password, user.encrypted_password)) {
-    return { valid: true, token: generateJWT(user), first_name: user.first_name };
+    const token = generateJWT(user);
+    // set cookie
+    const sevenDaysToSeconds = 7 * 24 * 60 * 60;
+    res.cookie('session', token,
+      {
+        maxAge: sevenDaysToSeconds,
+        httpOnly: false,
+        domain: process.env.NODE_ENV === 'production' ? '.bonde.org' : '.staging.bonde.org',
+        secure: process.env.NODE_ENV === 'production' ? true : false,
+      });
+    console.log("set-cookie", res);
+
+    return { valid: true, token, first_name: user.first_name };
   }
 
   throw new Error(errorCode);
