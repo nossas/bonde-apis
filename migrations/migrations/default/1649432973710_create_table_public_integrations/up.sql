f2529394f0704582771afd34f23ac15e951ec092
Adriele A Secundino <dri.leela@gmail.com>
2022-04-08 13:00:36 -0300

    chore(migrations): add integrations tables

--- migrations/migrations/default/1649432973710_create_table_public_integrations/up.sql
+++ migrations/migrations/default/1649432973710_create_table_public_integrations/up.sql
@@ -7,5 +7,4 @@ create table "public"."integrations"  (
 	"credentials" jsonb not null,
 	CONSTRAINT "integrations_pkey" PRIMARY KEY ("id"),
 	CONSTRAINT "integrations_community_id_fkey" FOREIGN KEY ("community_id") REFERENCES "public"."communities"("id") ON DELETE RESTRICT ON UPDATE RESTRICT
-
 );COMMENT ON TABLE "public"."integrations" IS E'Tabela responsável por armazenar informações das configurações das integrações usadas numa comunidade.';
