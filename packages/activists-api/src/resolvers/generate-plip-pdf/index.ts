592a300ce0ed4d2e75e6cc61bf12eecdc33ab6a9
Gloria Teodoro <gloriateodoro1@gmail.com>
2022-06-14 16:54:46 -0300

    update pdf

--- packages/activists-api/src/resolvers/generate-plip-pdf/index.ts
+++ packages/activists-api/src/resolvers/generate-plip-pdf/index.ts
@@ -45,8 +45,8 @@ const generatePlipPdf = async (unique_identifier: string, state: string, expecte
     protegidas e em florestas públicas não destinadas nas condições que especifica e dá outras providências. \nSaiba mais em amazoniadepe.org.br`, 220, 22, { align: 'center' });
     doc.setFontSize(6);
     doc.setFont("helvetica", 'normal');
-    doc.cell(margin, 62, (formWidth / 2), 12, `ESTADO: ${state}`, 0, 'left');
-    doc.cell(formWidth, 62, formWidth / 2, 12, `MUNICÍPIO:`, 0, 'left');
+    doc.cell(margin, 62, 100, 12, `ESTADO: ${state}`, 0, 'left');
+    doc.cell(formWidth, 62, formWidth - cellSignatureWidth, 12, `MUNICÍPIO:`, 0, 'left');
     doc.cell(margin, 100, 90, 5, ``, 1, 'left');
 
     //background color
@@ -56,34 +56,35 @@ const generatePlipPdf = async (unique_identifier: string, state: string, expecte
       doc.rect(10, top, formWidth, cellSignatureHeight, "F");
       top = top + (2 * cellSignatureHeight);
     }
-
     let barTop = 133
     for (let i = 0; i < 10; i++) {
       doc.setFontSize(10);
-      doc.text('            /            /            ', 10, barTop);
+      doc.text('            /            /            ', 110, barTop);
       barTop = barTop + 3 * cellHeight;
 
       doc.setFontSize(6);
+      doc.cell(margin, 134, 100, cellSignatureHeight,
+        `ASSINATURA OU IMPRESSÃO DIGITAL`, 2, 'left');
+
+
       doc.cell(margin, 110, (formWidth - cellSignatureWidth),
-        cellHeight, `NOME COMPLETO (Por extenso e legível, sem abreviar):`, 2, 'left');
+        cellHeight, `NOME COMPLETO (Por extenso e legível, sem abreviar):`, 2, 'right');
+      doc.cell(margin, 134, (formWidth - cellSignatureWidth), cellHeight, '', 2, 'left');
 
-      doc.cell((formWidth - cellSignatureWidth), 110, 100, cellSignatureHeight,
-        `ASSINATURA OU IMPRESSÃO DIGITAL`, 2, 'right');
-      doc.cell(margin, 134, (formWidth - cellSignatureWidth), cellHeight, ``, 2, 'left');
 
-      doc.cell(margin, 134, (formWidth - cellSignatureWidth),
-        cellHeight, `ENDEREÇO (Completo, legível, sem abreviar, com CEP):`, 3, 'left');
+      doc.cell(cellSignatureWidth + margin, 134, (formWidth - cellSignatureWidth),
+        cellHeight, `ENDEREÇO (Completo, legível, sem abreviar, com CEP):`, 3, 'right');
 
-      doc.cell(margin, 158, (formWidth - cellSignatureWidth) / 4,
+      doc.cell(cellSignatureWidth + margin, 158, (formWidth - cellSignatureWidth) / 4,
         cellHeight, `DATA DE NASCIMENTO:`, 4, 'center');
 
       doc.cell((formWidth - cellSignatureWidth) / 4, 158,
         (formWidth - cellSignatureWidth) - ((formWidth - cellSignatureWidth) / 2),
-        cellHeight, `NÚMERO DO TÍTULO DE ELEITOR (Ou nome completo da mãe):`, 4, 'left');
+        cellHeight, `NÚMERO DO TÍTULO DE ELEITOR (OU NOME COMPLETO DA MÃE):`, 4, 'center');
 
       doc.cell((formWidth - cellSignatureWidth) - ((formWidth - cellSignatureWidth) / 2), 158,
         (formWidth - cellSignatureWidth) / 4,
-        cellHeight, `WHATSAPP (Com DDD):`, 4, 'left');
+        cellHeight, `WHATSAPP (Com DDD):`, 4, 'right');
     }
 
     //footer
