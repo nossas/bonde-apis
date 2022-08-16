a3c95db0b25091424af21a70325b65165e51effa
Igor Santos <igr.exe@gmail.com>
2022-08-16 16:10:09 -0300

    feat(an): add query to fetch activist actions

--- action-network/an/database/bigquery.py
+++ action-network/an/database/bigquery.py
@@ -19,3 +19,17 @@ def select_groups(project_id="data-bonde"):
     # Run a Standard SQL query
     sql = "SELECT * FROM analyze.groups g"
     return pandas_gbq.read_gbq(sql, project_id=project_id, dialect='standard')
+
+
+def select_activist_actions(start_date, end_date, project_id="data-bonde")
+    """Fetch activist actions table"""
+    sql = f'''
+    SELECT
+        *
+    FROM `analyze.activist_actions` aa
+    INNER JOIN `analyze.actions` a ON a.widget_id = aa.widget_id
+    INNER JOIN `analyze.groups` g ON g.community_id = a.community_id
+    WHERE aa.action_date >= {start_date} AND aa.action_date <= {end_date}
+    '''
+
+    return pandas_gbq.read_gbq(sql, project_id=project_id, dialect='standard')
