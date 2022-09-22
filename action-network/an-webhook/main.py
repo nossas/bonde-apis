e2e4bfe277e8a49e85fe556695a1ce5372670b27
Igor Santos <igr.exe@gmail.com>
2022-09-22 18:26:35 -0300

    feat(an): add build to webbhook

--- action-network/an-webhook/main.py
+++ action-network/an-webhook/main.py
@@ -38,6 +38,6 @@ def create_app(config=None):
 
 
 if __name__ == "__main__":
-    port= int(os.environ.get("PORT", 8000))
-    app= create_app()
+    port = int(os.environ.get("PORT", 8000))
+    app = create_app()
     app.run(host="0.0.0.0", port=port)
