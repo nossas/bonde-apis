10be428ec06d066635c08ab47ed8370b8f8ad6b3
Adriele A Secundino <dri.leela@gmail.com>
2022-12-15 17:48:09 -0300

    chore(migrations): add schema mapa_do_acolhimento and views of forms triage 2

--- migrations/migrations/default/1669994207011_create_views_mda_results_triage_forms/down.sql
+++ migrations/migrations/default/1669994207011_create_views_mda_results_triage_forms/down.sql
@@ -1,2 +1,5 @@
-DROP VIEW IF EXISTS public.mda_results_juridical_triage_form;
-DROP VIEW IF EXISTS public.mda_results_psychological_triage_form
+DROP VIEW IF EXISTS mapa_do_acolhimento.results_juridical_triage_1_form;
+DROP VIEW IF EXISTS mapa_do_acolhimento.results_psychological_triage_1_form;
+DROP VIEW IF EXISTS mapa_do_acolhimento.results_juridical_triage_2_form;
+DROP VIEW IF EXISTS mapa_do_acolhimento.results_psychological_triage_2_form;
+DROP SCHEMA IF NOT EXISTS "mapa_do_acolhimento";
