841ce2b55f5fb113960a6e09480100ce796e1444
Adriele A Secundino <dri.leela@gmail.com>
2023-02-23 16:38:43 -0300

    chore(migrations): revert add mda_form_answers to schema mapa_do_acolhimento

--- migrations/migrations/default/1669994207011_create_views_mda_results_triage_forms/down.sql
+++ migrations/migrations/default/1669994207011_create_views_mda_results_triage_forms/down.sql
@@ -2,6 +2,5 @@ DROP VIEW IF EXISTS mapa_do_acolhimento.results_juridical_triage_1_form;
 DROP VIEW IF EXISTS mapa_do_acolhimento.results_psychological_triage_1_form;
 DROP VIEW IF EXISTS mapa_do_acolhimento.results_juridical_triage_2_form;
 DROP VIEW IF EXISTS mapa_do_acolhimento.results_psychological_triage_2_form;
-ALTER TABLE mapa_do_acolhimento.mda_forms_answers  SET SCHEMA public; 
 DROP SCHEMA IF EXISTS "mapa_do_acolhimento";
 
