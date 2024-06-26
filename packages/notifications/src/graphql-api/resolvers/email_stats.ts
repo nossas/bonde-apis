0af05857aec0e171d18d11488e807124e7ad176b
Mario Sergio <mariopekenossalro@gmail.com>
2024-06-26 15:02:59 -0300

    feat(notification): fixed autofire query filter

--- packages/notifications/src/graphql-api/resolvers/email_stats.ts
+++ packages/notifications/src/graphql-api/resolvers/email_stats.ts
@@ -14,34 +14,31 @@ interface EmailEvent {
 
 interface EmailStatsResponse {
   events: EmailEvent[];
+  stats: {
+    open: number;
+    delivered: number;
+    bounced: number;
+    processed: number;
+    click: number;
+  };
 }
 
 interface EmailStatsArgs {
   widget_id: number;
-  mobilization_id?: number;
-  community_id?: number;
 }
 
 
 export default async (_: void, args: EmailStatsArgs): Promise<EmailStatsResponse> => {
   let categories: string[] = [];
 
-  if (args.widget_id) {
-    categories.push(`autofire-w${args.widget_id}`);
-  }
-  if (args.mobilization_id) {
-    categories.push(`autofire-m${args.mobilization_id}`);
-  }
-  if (args.community_id) {
-    categories.push(`autofire-c${args.community_id}`);
-  }
-
+  categories.push(`autofire-w${args.widget_id}`);
+ 
   const query = {
     bool: {
       must: [
         {
           terms: {
-            category: categories
+            "category.keyword": categories
           }
         },
         {
@@ -73,10 +70,18 @@ export default async (_: void, args: EmailStatsArgs): Promise<EmailStatsResponse
       sg_message_id: hit._source.sg_message_id,
       useragent: hit._source.useragent,
       ip: hit._source.ip,
-      url: hit._source.url
+      url: hit._source.url,
     }));
 
-    return { events: events };
+    const stats = {
+      open: events.filter(event => event.event === "open").length,
+      delivered: events.filter(event => event.event === "delivered").length,
+      bounced: events.filter(event => event.event === "bounced").length,
+      processed: events.filter(event => event.event === "processed").length,
+      click: events.filter(event => event.event === "click").length,
+    };
+
+    return { events, stats };
   } catch (error) {
     logger.error(`Failed to fetch email stats`);
     throw new Error(`Failed to fetch email stats`);
