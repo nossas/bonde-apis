d0432b5dc35075de1de13be6a5ca9391f603ca7c
Igor Santos <igr.exe@gmail.com>
2024-07-03 17:21:09 -0300

    fix(notifications): count messages and events with precision

--- packages/notifications/src/graphql-api/resolvers/email_stats.ts
+++ packages/notifications/src/graphql-api/resolvers/email_stats.ts
@@ -1,32 +1,30 @@
 import logger, { apmAgent } from "../../logger";
 import { client } from "../../core/elasticsearchdb";
 
-interface EmailEvent {
-  email: string;
-  event: string;
-  timestamp: number;
-  sg_event_id: string;
-  sg_message_id: string;
-  useragent: string;
-  ip: string;
-  url: string;
-}
 
 interface EmailStatsResponse {
-  events: EmailEvent[];
+  total: number;
   stats: {
-    open: number;
-    delivered: number;
-    bounced: number;
-    processed: number;
-    click: number;
-    total: number;
+    processed?: number;
+    delivered?: number;
+    open?: number;
+    unsubscribe?: number;
+    bounce?: number;
+    deferred?: number;
+    dropped?: number;
+    spamreport?: number;
+    click?: number;
   };
 }
 
 interface EmailStatsArgs {
   widget_id: number;
   category?: string;
+}
+
+interface Bucket {
+  key: 'processed' | 'delivered' | 'open' | 'unsubscribe' | 'bounce' | 'deferred' | 'dropped' | 'spamreport' | 'click';
+  doc_count: number;
 }
 
 
@@ -46,51 +44,65 @@ export default async (_: void, args: EmailStatsArgs): Promise<EmailStatsResponse
           terms: {
             "category.keyword": categories
           }
-        },
-        {
-          bool: {
-            should: [
-              { match: { event: "delivered" } },
-              { match: { event: "bounced" } },
-              { match: { event: "processed" } },
-              { match: { event: "click" } },
-              { match: { event: "open" } }
-            ]
-          }
         }
       ]
     }
   };
 
+  const aggs = {
+    messages_count: {
+      scripted_metric: {
+        params: {
+          fieldName: "sg_message_id"
+        },
+        init_script: "state.list = []",
+        map_script: `
+        if(params['_source'][params.fieldName] != null) 
+          state.list.add(params['_source'][params.fieldName]);
+        `,
+        combine_script: "return state.list;",
+        reduce_script: `
+        Map uniqueValueMap = new HashMap();
+        int count = 0;
+        for(shardList in states) {
+          if(shardList != null) {
+            for(key in shardList) {
+              if(!uniqueValueMap.containsKey(key)) {
+                count +=1;
+                uniqueValueMap.put(key, key); 
+              }
+            }
+          }
+        }
+        return count;
+        `
+      }
+    },
+    "events_count": {
+      "terms": {
+        "field": "event"
+      }
+    }
+  }
+
   try {
     const { body } = await client.search({
       index: "events-sendgrid-*",
-      body: { query: query }
+      body: { query, aggs }
     });
 
-    const events = body.hits.hits.map((hit: any) => ({
-      email: hit._source.email,
-      event: hit._source.event,
-      timestamp: hit._source.timestamp,
-      sg_event_id: hit._source.sg_event_id,
-      sg_message_id: hit._source.sg_message_id,
-      useragent: hit._source.useragent,
-      ip: hit._source.ip,
-      url: hit._source.url,
-    }));
+    const stats = {};
+    body.aggregations.events_count.buckets.forEach((item: Bucket) => {
+      stats[item.key] = item.doc_count
+    })
 
-    const stats = {
-      open: events.filter(event => event.event === "open").length,
-      delivered: events.filter(event => event.event === "delivered").length,
-      bounced: events.filter(event => event.event === "bounced").length,
-      processed: events.filter(event => event.event === "processed").length,
-      click: events.filter(event => event.event === "click").length,
-      total: events.length
+    return {
+      total: body.aggregations.messages_count.value,
+      stats
     };
-
-    return { events, stats };
   } catch (error) {
-    logger.error(`Failed to fetch email stats`);
+    console.log("error", error);
+    logger.error(`Failed to fetch email stats`);78152
     throw new Error(`Failed to fetch email stats`);
   }
 };
