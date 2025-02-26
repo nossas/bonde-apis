321dbb10ae82df60197c3a836d73824f4cd17a73
Igor Santos <igr.exe@gmail.com>
2025-02-26 15:24:23 -0300

    fix(packages): remove redes-api

--- packages/redes-api/src/types/recipient.ts
+++
@@ -1,8 +0,0 @@
-import { HasuraTicket } from "./ticket"
-import { VolunteerUser } from "./volunteer"
-
-export type UpdateRecipientTicket = {
-  recipient_ticket: Pick<HasuraTicket, 'organization_id' | 'nome_msr' | 'ticket_id'> 
-  volunteer_user: Pick<VolunteerUser, 'name' | 'ticket_id' | 'registration_number' | 'whatsapp' | 'phone'>
-  agent: number
-}
